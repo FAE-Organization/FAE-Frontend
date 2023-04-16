1e7ab10f0a284fc579f50b406be861689e73b3b9
PatrickLiu022 <patrickliu022@gmail.com>
2023-04-15 20:29:11 -0700

    wip

--- fae-frontend/components/ui/directory-grid.jsx
+++ fae-frontend/components/ui/directory-grid.jsx
@@ -1,6 +1,10 @@
-import { Stack, HStack, Box, Text, Badge } from "@chakra-ui/react"
+import { Stack, HStack, Box, Text, Badge, Image } from "@chakra-ui/react"
+import Link from "next/link"
+import { useState } from "react";
 
 export default function DirectoryGrid({ cards }) {
+    const [hoveredIndex, setHoveredIndex] = useState(-1);
+
     return (
         <Stack width='100%' alignItems='center'>
             <HStack
@@ -18,36 +22,57 @@ export default function DirectoryGrid({ cards }) {
                         borderRadius="md"
                         overflow="hidden"
                         mx='3px'
+                        onMouseEnter={() => setHoveredIndex(index)}
+                        onMouseLeave={() => setHoveredIndex(-1)}
+                        transition="transform 0.2s ease-in-out, filter 0.2s ease-in-out"
+                        transform={hoveredIndex === index ? "scale(1.03)" : ""}
                     >
-                        <Stack
-                            h="200px"
-                            bg={entry}
-                            justifyContent='space-between'
-                            padding='5px'
-                        >
-                            <Stack>
-                                <Badge
-                                    width={{ base: '75%', md: '60^', lg: '50%' }}
-                                    backgroundColor='purple.500' color='#FFF'
-                                    borderRadius='5px'
+                        <Link href={`/directory/category?id=${index}`}>
+                            <Stack
+                                h="200px"
+                                justifyContent='space-between'
+                                padding='5px'
+                                backgroundImage={`url(${'https://besthqwallpapers.com/Uploads/21-12-2019/116771/thumb-purple-neon-lights-black-background-purple-neon-light-neon-background.jpg'})`}
+                                backgroundPosition='center'
+                                backgroundSize='cover'
+                                transition='background-size 0.3s ease-in-out'
+                                filter={hoveredIndex === index ? "brightness(75%)" : ""}
+                                _hover={{
+                                    backgroundSize: '120%',
+                                    filter: "brightness(50%)",
+                                    transition: 'background-size 0.3s ease-in-out, filter 0.3s ease-in-out'
+                                }}
+                            >
+                                {/* _hover={{
+                                    transform: 'scale(1.5)',
+                                    transition: 'transform 0.3s ease-in-out, filter 0.3s ease-in-out'
+                                }} */}
+                                <Stack zIndex={1}>
+                                    <Badge
+                                        width={{ base: '75%', md: '60^', lg: '50%' }}
+                                        backgroundColor='purple.500' color='#FFF'
+                                        borderRadius='5px'
+                                    >
+                                        100+ profiles
+                                    </Badge>
+                                </Stack>
+                                <Stack
+                                    zIndex={1}
                                 >
-                                    100+ profiles
-                                </Badge>
+                                    <Text
+                                        fontWeight={700}
+                                        color='#FEFEFE'
+                                        fontSize='26px'
+                                    >
+                                        Title Goes Here
+                                    </Text>
+                                </Stack>
                             </Stack>
-                            <Stack>
-                                <Text
-                                    fontWeight={700}
-                                    color='#FEFEFE'
-                                    fontSize='26px'
-                                >
-                                    Title Goes Here
-                                </Text>
-                            </Stack>
-                        </Stack>
-                        <Box p="20px">
-                            <Text fontWeight="semibold">Card {index + 1} </Text>
-                            <Text>Some text here.</Text>
-                        </Box>
+                            <Box p="20px">
+                                <Text fontWeight="semibold">Card {index + 1} </Text>
+                                <Text>Some text here.</Text>
+                            </Box>
+                        </Link>
                     </Stack>
                 ))}
             </HStack>
