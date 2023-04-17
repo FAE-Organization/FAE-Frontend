01cbc7f85b2d31e0b534f8cbc1fc28548e7d4aa5
PatrickLiu022 <patrickliu022@gmail.com>
2023-04-16 18:26:18 -0700

    added responsiveness to page title

--- fae-frontend/pages/directory/index.jsx
+++ fae-frontend/pages/directory/index.jsx
@@ -1,3 +1,4 @@
+import { Stack } from "@chakra-ui/react";
 import CenteredTitle from "@/components/ui/centered-title";
 import DirectoryGrid from "@/components/ui/directory-grid";
 import React from "react";
@@ -5,13 +6,15 @@ import React from "react";
 export default function Directory(props) {
     return (
         <React.Fragment>
-            <CenteredTitle
-                title={"Explore our Directory"}
-                description={
-                    "Our directory is designed to help users understand the different sectors that make up the esports and gaming industry."
-                }
-            />
-            <DirectoryGrid cards={['blue.700', 'green.700', 'red.700', 'purple.700', 'yellow.700', 'pink.700']} />
+            <Stack gap='20px'>
+                <CenteredTitle
+                    title={"Explore our Directory"}
+                    description={
+                        "Our directory is designed to help users understand the different sectors that make up the esports and gaming industry."
+                    }
+                />
+                <DirectoryGrid cards={['blue.700', 'green.700', 'red.700', 'purple.700', 'yellow.700', 'pink.700']} />
+            </Stack>
         </React.Fragment >
     )
 }
