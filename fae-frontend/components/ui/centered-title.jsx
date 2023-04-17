f7099d5728b92f2ad091aff09f42381670afeab9
PatrickLiu022 <patrickliu022@gmail.com>
2023-04-16 18:26:18 -0700

    added responsiveness to page title

--- fae-frontend/components/ui/centered-title.jsx
+++ fae-frontend/components/ui/centered-title.jsx
@@ -4,16 +4,20 @@ export default function CenteredTitle({ title, description }) {
     return (
         <Stack
             width='100%'
-            alignItems='align-center'
+            alignItems='center'
+            padding='20px'
         >
-            <Stack width='80%'>
+            <Stack
+                width={{ base: '90%', md: '75%' }}
+                textAlign='center'
+            >
                 <Text
                     fontWeight={700}
-                    fontSize='20px'
+                    fontSize={{ base: '26px', md: '32px', lg: '35px' }}
                 >
                     {title}
                 </Text>
-                <Text>
+                <Text fontSize={{ base: '16px', md: '18px', lg: '20px' }}>
                     {description}
                 </Text>
             </Stack>
