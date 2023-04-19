031b5a50203d057de08690cd93de51a55e6784e4
PatrickLiu022 <patrickliu022@gmail.com>
2023-04-19 00:46:37 -0700

    finished search page rough draft

--- fae-frontend/components/ui/user-cards.jsx
+++ fae-frontend/components/ui/user-cards.jsx
@@ -12,56 +12,102 @@ export default function UserCards({ cards }) {
 
     return (
         <Stack width='100%'>
-            <Grid templateColumns='repeat(4, 1fr)' gap='20px'>
+            <Grid
+                templateColumns={{ base: '1fr', md: 'repeat(2, 1fr)', xl: 'repeat(4, 1fr)' }}
+                gap='20px'
+            >
                 {cardVals.map(card => (
-                    <GridItem key={card.id} height='600px' boxShadow='1px 1px 15px #888'>
+                    <GridItem key={card.id} height='515px' boxShadow='1px 1px 15px #888'>
                         <Stack>
                             {/* Image field will be added to the cards as well */}
                             <Image
                                 src='https://besthqwallpapers.com/Uploads/21-12-2019/116771/thumb-purple-neon-lights-black-background-purple-neon-light-neon-background.jpg'
                                 alt={`Profile image for ${card.username}`}
+                                borderTopLeftRadius='5px'
+                                borderTopRightRadius='5px'
                             />
                         </Stack>
                         <Stack padding='10px'>
-                            <HStack>
-                                <Text>{card.username}</Text>
-                                <Text>{`(${card.pronouns.join('/')})`}</Text>
+                            <HStack alignItems='center'>
+                                <Text fontSize='22px' fontWeight={600}>{card.username}</Text>
+                                <Text fontSize='12px' color='#8F8F8F'>{`(${card.pronouns.join('/')})`}</Text>
                             </HStack>
-                            <Stack height='150px' justifyContent='space-between'>
+                            <Stack height='130px' justifyContent='space-between'>
                                 <Stack>
                                     <Text>Roles</Text>
-                                    <HStack flexWrap='wrap' gap='8px' justifyContent='flex-start'>
+                                    <HStack flexWrap='wrap' gap='10px' spacing={0} justifyContent='flex-start'>
                                         {card.roles.map((entry, index) => (
-                                            <Badge key={index} marginLeft='8px' fontSize='10px'>{entry}</Badge>
+                                            <Badge
+                                                key={index}
+                                                fontSize='10px'
+                                                backgroundColor={(() => {
+                                                    switch (entry) {
+                                                        case 'observer':
+                                                            return 'red.100'
+                                                        case 'graphic designer':
+                                                            return 'pink.100'
+                                                        case 'tournament admin':
+                                                            return 'blue.100'
+                                                        case 'producer':
+                                                            return 'green.100'
+                                                    }
+                                                })()}
+                                            >
+                                                {entry}
+                                            </Badge>
                                         ))}
                                     </HStack>
                                 </Stack>
                                 <Stack>
                                     <Text>Region</Text>
-                                    <Stack pl='8px'>
-                                        <Badge width='fit-content'>{card.region}</Badge>
+                                    <Stack>
+                                        <Badge
+                                            width='fit-content'
+                                            backgroundColor={(() => {
+                                                switch (card.region) {
+                                                    case 'NA':
+                                                        return 'purple.200'
+                                                    case 'SA':
+                                                        return 'yellow.400'
+                                                    case 'AS':
+                                                        return 'blue.200'
+                                                }
+                                            })()}
+                                        >
+                                            {card.region}
+                                        </Badge>
                                     </Stack>
                                 </Stack>
                             </Stack>
-                            <Stack height='125px'>
-                                <Text>Notable Events</Text>
-                                <Stack>
+                            <Stack height='75px' padding='10px 0px'>
+                                <Text fontSize='12px' fontWeight={600}>NOTABLE EVENTS</Text>
+                                <Stack lineHeight='10px' fontSize='12px'>
                                     {card.notableEvents.map((events, index) => (
                                         <Text key={index}>{events}</Text>
                                     ))}
                                 </Stack>
                             </Stack>
                             <Stack>
-                                <Text>Tags</Text>
-                                <HStack flexWrap='wrap' gap='8px'>
+                                <Text
+                                    color='#505050'
+                                    fontWeight={600}
+                                    fontSize='12px'
+                                >
+                                    TAGS
+                                </Text>
+                                <HStack
+                                    flexWrap='wrap'
+                                    gap='10px'
+                                    spacing={0}
+                                >
                                     {card.tags.map((entry, index) => (
-                                        <Badge key={index} mx='8px'>{entry}</Badge>
+                                        <Badge key={index} fontSize='10px'>{entry}</Badge>
                                     ))}
                                 </HStack>
                             </Stack>
-                            <HStack>
+                            <HStack fontWeight={700}>
                                 <Icon as={BiDollarCircle} color='#7BBB9C' />
-                                <Text>{card.compensation.amount}/{card.compensation.type}.</Text>
+                                <Text>{card.compensation.amount}/{card.compensation.type}</Text>
                             </HStack>
                         </Stack>
                     </GridItem>
