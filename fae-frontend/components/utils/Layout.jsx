acbd673a7e67b7480927608dc57250a5aec1afbd
elorahob <elorahob@uw.edu>
2023-04-27 23:05:58 -0700

    Initialize profile page

--- fae-frontend/components/utils/Layout.jsx
+++ fae-frontend/components/utils/Layout.jsx
@@ -1,16 +1,17 @@
 import { Stack } from "@chakra-ui/react";
-import NavBar from "./NavBar.jsx";
+import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 export default function Layout({ children }) {
     return (
+        // Styling in <Stack> impacts ENTIRE application !!!!!
         <Stack
             fontFamily={'Poppins'}
             color="black"
             backgroundColor='#F5F5F5'
         >
-            <NavBar />
-            {children}
+            <Navbar />
+                {children}
             <Footer />
         </Stack>
     )
