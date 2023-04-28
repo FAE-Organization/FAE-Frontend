8085006858023b1b9fa2561e69ab8d1e1a310d1c
PatrickLiu022 <patrickliu022@gmail.com>
2023-04-28 16:39:38 -0700

    wip dropdown category

--- fae-frontend/components/temp/lib/functions/cacheValidation.js
+++
@@ -1,4 +0,0 @@
-export function cacheValidation(expirationDate) {
-    const currentTime = new Date().getTime()
-    return currentTime > new Date(expirationDate).getTime()
-}
--- fae-frontend/components/temp/lib/functions/capitalizeFirstWord.js
+++
@@ -1,8 +0,0 @@
-export const capitalizeFirstWord = (text) => {
-    const words = text.split(" "); // Split the string into words
-    const capitalizedWords = words.map((word) => {
-        return word.charAt(0).toUpperCase() + word.slice(1); // Capitalize the first character of each word
-    });
-    const capitalizedText = capitalizedWords.join(" "); // Join the words back into a string
-    return capitalizedText;
-};
