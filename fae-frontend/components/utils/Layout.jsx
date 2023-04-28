8372ef5027bfd3005f8482cce9719d21d4c1f81b
elorahob <elorahob@uw.edu>
2023-04-27 21:20:17 -0700

    Adjust NavBar styling & Adjust font styling

--- fae-frontend/components/utils/Layout.jsx
+++ fae-frontend/components/utils/Layout.jsx
@@ -4,7 +4,6 @@ import Footer from "./Footer";
 
 export default function Layout({ children }) {
     return (
-        // Styling in <Stack> impacts ENTIRE application !!!!!
         <Stack
             fontFamily={'Poppins'}
             color="black"
