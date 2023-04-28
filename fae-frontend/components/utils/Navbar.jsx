66314659f0717abb0836d349a395a0ad7aed410a
elorahob <elorahob@uw.edu>
2023-04-27 23:09:41 -0700

    Include draft Navbar

--- fae-frontend/components/utils/Navbar.jsx
+++ fae-frontend/components/utils/Navbar.jsx
@@ -1,28 +1,81 @@
-import { Stack, HStack } from "@chakra-ui/react"
+// import { Stack, HStack } from "@chakra-ui/react"
+import { Flex, Box, Spacer, Link, Button } from '@chakra-ui/react';
 
 export default function Navbar() {
-    return (
-        <Stack
-            width='100%'
-            height='100px'
-            alignItems='center'
-            backgroundColor='white'
-        >
-            <HStack width='95%' justifyContent='space-between'>
-                <Stack>
-                    {/* 
-                    Logo + Navigation Items.
-                    
-                    Probably going to set up CMS so we can update the logo + nav items anytime we need to.
-                    Saves time and separates UI/UX from code
-                     */}
-                </Stack>
-                <Stack>
-                    {/* 
-                    Username and Profile Link
-                    */}
-                </Stack>
-            </HStack>
-        </Stack>
-    )
+  return (
+    <Flex
+      as="nav"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      px={4}
+      py={2}
+      bg="#FFFFFF"
+      color="black"
+    >
+      <Flex align="center" mr={5}>
+        <Box p={2} bg="white">
+          <Link href="/">
+            {/* TODO: Might need to adjust height and width later */}
+            <img src="logo/fae-logo.png" alt="Logo" height="auto" width="150" />
+          </Link>
+        </Box>
+        <Box>
+          <Link href="/directory">
+            <Button
+              variant="ghost"
+              color="black"
+              fontSize={{ base: '20px', md: '30px', lg: '40px' }}
+              onClick={() => router.push('/directory')} >
+              Directory
+            </Button>
+          </Link>
+          <Link href="/search">
+            <Button
+              variant="ghost"
+              color="black"
+              fontSize={{ base: '20px', md: '30px', lg: '40px' }} >
+              Search
+            </Button>
+          </Link>
+          <Link href="/about">
+            <Button
+              variant="ghost"
+              color="black"
+              fontSize={{ base: '20px', md: '30px', lg: '40px' }} >
+              About
+            </Button>
+          </Link>
+        </Box>
+      </Flex>
+      <Spacer />
+      <Box>
+        {/* Username and Profile Link */}
+      </Box>
+    </Flex>
+  );
 }
+
+
+// <Stack
+        //     width='100%'
+        //     height='100px'
+        //     alignItems='center'
+        //     backgroundColor='white'
+        // >
+        //     <HStack width='95%' justifyContent='space-between'>
+        //         <Stack>
+        //             {/* 
+        //             Logo + Navigation Items.
+                    
+        //             Probably going to set up CMS so we can update the logo + nav items anytime we need to.
+        //             Saves time and separates UI/UX from code
+        //              */}
+        //         </Stack>
+        //         <Stack>
+        //             {/* 
+        //             Username and Profile Link
+        //             */}
+        //         </Stack>
+        //     </HStack>
+        // </Stack>
