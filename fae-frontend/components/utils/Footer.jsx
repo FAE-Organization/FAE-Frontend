25fc1c3a944954d458e8dfa76450051df11fed3b
elorahob <elorahob@uw.edu>
2023-04-29 09:24:48 -0700

    Implement footer styling

--- fae-frontend/components/utils/Footer.jsx
+++ fae-frontend/components/utils/Footer.jsx
@@ -1,15 +1,63 @@
-import { Stack } from "@chakra-ui/react";
+import { Stack, Text, Box, HStack, Link, Button, Image} from "@chakra-ui/react";
 
 export default function Footer() {
     return (
         <Stack
             width='100%'
-            height='100px'
-            alignItems='center'
-            backgroundColor='#EEE'
+            backgroundColor='#F5F5F5'
         >
-            <Stack width='95%'>
+            <Stack alignItems='center'>
+                <Box>
+                    <Image
+                        src="logo/fae-logo.png"
+                        alt="For Anything Esports logo"
+                        // height="auto"
+                        // width="150"
+                        width={{ base: '150px', lg: '160px', xl: '180px' }}
 
+                    />
+                </Box>
+                <HStack>
+                    <Link href="/">
+                        <Button
+                            // TODO: Create theme component to style these buttons &
+                            //       simplify this duplicated code
+                            variant="ghost"
+                            color="black"
+                            fontSize={{ base: '15px', lg: '20px', xl: '25px' }}
+                            fontWeight="semibold"
+                            onClick={() => router.push('/')} >
+                            About
+                        </Button>
+                    </Link>
+                    <Link href="/">
+                        <Button
+                            variant="ghost"
+                            color="black"
+                            fontSize={{ base: '15px', lg: '20px', xl: '25px' }}
+                            fontWeight="semibold"
+                            onClick={() => router.push('/')} >
+                            Privacy Policy
+                        </Button>
+                    </Link>
+                    <Link href="/">
+                        <Button
+                            variant="ghost"
+                            color="black"
+                            fontSize={{ base: '15px', lg: '20px', xl: '25px' }}
+                            fontWeight="semibold"
+                            onClick={() => router.push('/')} >
+                            FAQ
+                        </Button>
+                    </Link>
+                </HStack>
+                <Text 
+                    pb={{ base: '15', md: '20', xl: '25' }}
+                    fontSize={{ base: '15px', lg: '20px' }}
+                    fontWeight='light'
+                >
+                    Copyright © 2023 FAE - For Anything Esports
+                </Text>
             </Stack>
         </Stack>
     )
