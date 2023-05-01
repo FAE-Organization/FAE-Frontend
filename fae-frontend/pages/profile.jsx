0a04e6f7549e225bf1ade0550934acadac8ccaf9
elorahob <elorahob@uw.edu>
2023-05-01 16:36:50 -0700

    make minor styling changes on profile page

--- fae-frontend/pages/profile.jsx
+++ fae-frontend/pages/profile.jsx
@@ -1,15 +1,13 @@
 import ProfileBody from "@/components/ui/profile/ProfileBody/profile-body";
 import Showcase from "@/components/ui/profile/WorkShowcase/showcase";
 import React from "react";
-import { VStack, HStack, Center } from "@chakra-ui/react";
+import { VStack } from "@chakra-ui/react";
 import UserBanner from "@/components/ui/profile/UserBanner/user-banner";
 
-export default function Profile(props) {
+export default function Profile() {
     return (
         <React.Fragment>
-                <VStack
-                    padding='50px'
-                >
+                <VStack padding='50px'>
                     <UserBanner />
                     <ProfileBody />
                 </VStack>
