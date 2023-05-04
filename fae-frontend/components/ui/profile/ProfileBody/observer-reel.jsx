d5cee3ee7eb7540b5cc6f88267d2a56dba871f7b
PatrickLiu022 <patrickliu022@gmail.com>
2023-05-03 21:20:22 -0700

    LMAO GG

--- fae-frontend/components/ui/profile/ProfileBody/observer-reel.jsx
+++ fae-frontend/components/ui/profile/ProfileBody/observer-reel.jsx
@@ -1,16 +1,16 @@
 import { Image, Stack, Box } from "@chakra-ui/react";
 import Subheader from "@/components/ui/profile/ProfileBody/subheader"
 
-export default function ObserverReel() {
+export default function ObserverReel({ index }) {
     return (
         <Stack>
-                <Subheader
-                    category='Observer Reel' // TODO: may need to refactor this later.
-                />
+            <Subheader
+                category='Observer Reel' // TODO: may need to refactor this later.
+            />
 
-                <Image // This may be changed later
-                    src='/reelMockImage.png'
-                />
+            <Image // This may be changed later
+                src={index === 1 ? '/reelMockImage.png' : '/reelMockImage3.png'}
+            />
         </ Stack>
     )
 }
