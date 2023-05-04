e2f48e4906d048275122364a1c0766f33a1a9f49
elorahob <elorahob@uw.edu>
2023-05-03 22:28:26 -0700

    Trying to fix things I'm so sorry

--- fae-frontend/components/ui/profile/UserBanner/user-banner.jsx
+++ fae-frontend/components/ui/profile/UserBanner/user-banner.jsx
@@ -1,34 +1,31 @@
-import { VStack, HStack, Image } from "@chakra-ui/react";
+import { Spacer, Box, Image, Flex } from "@chakra-ui/react";
 import UserHeader from "./user-header.jsx";
 import UserCategories from "./user-categories.jsx";
 import UserBio from "./user-bio.jsx";
 
 export default function UserBanner({ roles, tags, region, image, username, pronouns, payrate }) {
     return (
-        <HStack
-            align='top'
-            maxWidth='100%'
-        >
-
-            <Image
-                src={image === 1 ? '/userMockIcon_1.png' : '/userMockIcon_2.png'}
-                borderRadius='20px'
-                width='300px'
-                mt='5px'
-                me='20px'
-            />
-
-            <VStack
-                align='left'
-            >
-                <UserHeader
-                    username={username}
-                    pronouns={pronouns}
-                    payrate={payrate}
+        <Flex direction={'row'}>
+            <Box>
+                <Image
+                    src={image === 1 ? '/userMockIcon_1.png' : '/userMockIcon_2.png'}
+                    borderRadius='20px'
+                    mt='5px'
+                    me='20px'
                 />
-                <UserCategories roles={roles} tags={tags} region={region} />
-                <UserBio />
-            </VStack>
-        </HStack>
+            </Box>
+            <Spacer />
+            <Box>
+                <Flex align='left' direction={'column'}>
+                    <UserHeader
+                        username={username}
+                        pronouns={pronouns}
+                        payrate={payrate}
+                    />
+                    <UserCategories roles={roles} tags={tags} region={region} />
+                    <UserBio />
+                </Flex>
+            </Box>
+        </Flex>
     )
 }
