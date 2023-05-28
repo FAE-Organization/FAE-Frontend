d84f210af616045d1a72432579bb2f11650cd637
elorahob <elorahob@uw.edu>
2023-05-27 20:34:24 -0700

    Initialize user redux profile slice

--- fae-frontend/lib/redux/store.js
+++ fae-frontend/lib/redux/store.js
@@ -3,6 +3,7 @@ import { activeBadgeSlice } from "./activeBadgeSlice";
 import { filterSubcategorySlice } from "./filterSubcategorySlice";
 import { formSlice } from "./formSlice";
 import { loadingSlice } from "./loadingSlice";
+import userProfileSlice from "./userProfileSlice";
 import { userSlice } from "./userSlice";
 
 export const store = configureStore({
@@ -11,6 +12,7 @@ export const store = configureStore({
         user: userSlice.reducer,
         filterSubcategoryDoNotChange: filterSubcategorySlice.reducer,
         loading: loadingSlice.reducer,
-        activeBadge: activeBadgeSlice.reducer
+        activeBadge: activeBadgeSlice.reducer,
+        user: userProfileSlice.reducer, 
     },
 })
--- fae-frontend/lib/redux/userProfileSlice.js
+++ fae-frontend/lib/redux/userProfileSlice.js
@@ -35,8 +35,83 @@ export const userProfileSlice = createSlice({
     name: 'userProfile',
     initialState,
     reducers: {
-        setUserProfile: (state, action) => {
-            state.userProfileData = action.payload
+        // setUserProfile: (state, action) => {
+        //     state.userProfileData = action.payload
+        // },
+        setId: (state, action) => {
+            state.id = action.payload
+        },
+        setUsername: (state, action) => {
+            state.username = action.payload
+        },
+        setBio: (state, action) => {
+            state.bio = action.payload
+        },
+        setPronouns: (state, action) => {
+            state.pronouns = action.payload
+        },
+        setTwitch: (state, action) => {
+            state.twitch= action.payload
+        },
+        setYoutube: (state, action) => {
+            state.youtube = action.payload
+        },
+        setDiscord: (state, action) => {
+            state.discord = action.payload
+        },
+        setTwitter: (state, action) => {
+            state.twitter = action.payload
+        },
+        setProfilePic: (state, action) => {
+            state.profilePic = action.payload
+        },
+        setEmail: (state, action) => {
+            state.email = action.payload
+        },
+        setSalary: (state, action) => {
+            const { currency, compensationType, amount} = action.payload;
+            state.salary.amount = amount
+            state.salary.currency = currency
+            state.salary.compensationType = compensationType
+        },
+        setGame: (state, action) => {
+            state.game = action.payload
+        },
+        setRegion: (state, action) => {
+            state.region = action.payload
+        },
+        setExperience: (state, action) => {
+            state.experience = action.payload
+        },
+        setSiteType: (state, action) => {
+            state.siteType = action.payload
+        },
+        setTags: (state, action) => {s
+            state.tags = action.payload
+        },
+        setRoles: (state, action) => {
+            state.roles = action.payload
+        },
+        setShowcase: (state, action) => {
+            state.showcase = action.payload
+        },
+        setDesign: (state, action) => {
+            state.design = action.payload
+        },
+        setEvents: (state, action) => {
+            state.events = action.payload
+        },
+        setArticles: (state, action) => {
+            state.articles = action.payload
+        },
+        setObserver: (state, action) => {
+            state.observer = action.payload
+        },
+        setEditing: (state, action) => {
+            state.editing = action.payload
+        },
+        setCasting: (state, action) => {
+            state.casting = action.payload
         },
     }
 })
